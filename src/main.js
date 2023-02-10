46baf6c39c139fc915291663bd76817f6c1b13a7
mamadousaliou-bah <bahs2592@gmail.com>
2023-02-10 15:46:34 +0100

    modification de la structure et des mawnha

---
+++ src/main.js
@@ -0,0 +1,338 @@
+const data = [
+  {
+    name: "Reincarnation of the Suicidal Battle God",
+    released: "2017-04-27",
+    metacritic: 90,
+    background_image: "images/Reincarnation_of_the_Suicidal_Battle_God.jpg",
+  },
+  {
+    name: "The Constelation That Returned From Hell",
+    released: "2022-11-09",
+    metacritic: 90,
+    background_image: "images/The_Constelation_That_Returned_From_Hell.jpg",
+  },
+  {
+    name: "I Reincarnated As The Crazed Heir",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/I_Reincarnated_As_The_Crazed_Heir.jpg",
+  },
+  {
+    name: "My Dad Is Too Strong",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/dadstrong.jpg",
+  },
+  {
+    name: "Damn Reincarnation",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Damn_Reincarnation.jpg",
+  },
+  {
+    name: "The Dark Magician Transmigrates After 66666 Years",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image:
+      "images/The_Dark_Magician_Transmigrates_After_66666_Years.jpg",
+  },
+  {
+    name: "Heavenly Demon Instructor",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Heavenly_Demon_Instructor.jpg",
+  },
+  {
+    name: "The Return of the Disaster-Class Hero",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/The_Return_of_the_Disaster-Class_Hero.jpg",
+  },
+  {
+    name: "My Daughter is the Final Boss",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/My_Daughter_is_the_Final_Boss.jpg",
+  },
+  {
+    name: "The Heavenly Demon Can't Live a Normal Life",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/The_Heavenly_Demon_Can’t_Live_a_Normal_Life.jpg",
+  },
+  {
+    name: "The Max Level Hero Has Returned",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/The_Max_Level_Hero_has_Returned.jpg",
+  },
+  {
+    name: "Return of The Mount Hua Sect",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Return_of_The_Mount_Hua_Sect.jpg",
+  },
+  {
+    name: "Nano Machine",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/nanomachine.jpg",
+  },
+  {
+    name: "Seoul Station's Necromancer",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/necromancer.jpg",
+  },
+  {
+    name: "Overgeared",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/overgeared.jpg",
+  },
+  {
+    name: "I Regressed As The Duke",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/I_Regressed_As_The_Duke.jpg",
+  },
+  {
+    name: "Second Life Ranker",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/secondtime.jpg",
+  },
+  {
+    name: "Return of The Unrivaled Spear Knight",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Return_of_The_Unrivaled_Spear_Knight.jpg",
+  },
+  {
+    name: "I Became a Renowned Family's Sword Prodigy",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/I_Became_a_Renowned_Family’s_Sword_Prodigy.jpg",
+  },
+  {
+    name: "Talent-Swallowing Magician",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Talent-Swallowing_Magician.jpg",
+  },
+  {
+    name: "The Lord Coins",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/The_Lord_Coins.jpg",
+  },
+  {
+    name: "Academy Undercover Professor",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Academy_Undercover_Professor.jpg",
+  },
+  {
+    name: "Swordmaster Youngest Son",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Swordmaster_Youngest_Son.jpg",
+  },
+  {
+    name: "The Greatest Estate Developer",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/The_Greatest_Estate_Developer.jpg",
+  },
+  {
+    name: "Reaper of the Drifting Moon",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Reaper_of_the_Drifting_Moon.jpg",
+  },
+  {
+    name: "Heavenly Inquisition Sword",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Heavenly_Inquisition_Sword.jpg",
+  },
+  {
+    name: "Standard of Reincarnation",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Standard_of_Reincarnation.jpg",
+  },
+  {
+    name: "How To Live As a Villain",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/How_To_LiveAs_a_Villain.jpg",
+  },
+  {
+    name: "Omniscient Reader’s Viewpoint",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Omniscient_Reader’s_Viewpoint.jpg",
+  },
+  {
+    name: "Murim Login",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Murim_Login.jpg",
+  },
+  {
+    name: "The S-Classes That I Raised",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/The_S-Classes_That_I_Raised.jpg",
+  },
+  {
+    name: "Warrior High School – Dungeon Raid Department",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Warrior_High_School–Dungeon_Raid_Department.jpg",
+  },
+  {
+    name: "The World After The End",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/The_World_After_The_End.jpg",
+  },
+  {
+    name: "Player Who Can’t Level Up",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Player_Who_Can’t_Level_Up.jpg",
+  },
+  {
+    name: "I Obtained a Mythic Item",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/I_Obtained_a_Mythic_Item.jpg",
+  },
+  {
+    name: "Player Who Returned 10 000 Years Later",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Player_Who_Returned_10_000_Years_Later.jpg",
+  },
+  {
+    name: "Boundless Necromancer",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Boundless_Necromancer.jpg",
+  },
+  {
+    name: "Return of the SSS-Class Ranker",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Return_of_the_SSS-Class_Ranker.jpg",
+  },
+  {
+    name: "Poison-Eating Healer",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Poison-Eating_Healer.jpg",
+  },
+  {
+    name: "Champion Path to Murim",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Champion_Path_to_Murim.jpg",
+  },
+  {
+    name: "Demon Lord Martial Arts Ascension",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Demon_Lord_Martial_Arts_Ascension.jpg",
+  },
+  {
+    name: "Regressor Instruction Manual",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Regressor_Instruction_Manual.jpg",
+  },
+  {
+    name: "Star Instructor Master Baek",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Star_Instructor_Master_Baek.jpg",
+  },
+  {
+    name: "Wandering Warrior of Wudang",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Wandering_Warrior_of_Wudang.jpg",
+  },
+  {
+    name: "The Novel Extra (Remake)",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/The_Novel_Extra_(Remake).jpg",
+  },
+  {
+    name: "Sleeping Ranker",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Sleeping_Ranker.jpg",
+  },
+  {
+    name: "Return Of The Shattered Constellation",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Return_Of_The_Shattered_Constellation.jpg",
+  },
+  {
+    name: "Heavenly Sword Grand Saga",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Heavenly_Sword_Grand_Saga.jpg",
+  },
+  {
+    name: "The Hero Returns",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/The_Hero_Returns.jpg",
+  },
+  {
+    name: "Heavenly Martial God",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Heavenly_Martial_God.jpg",
+  },
+  {
+    name: "Maxed Out Leveling",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/Maxed_Out_Leveling.jpg",
+  },
+  {
+    name: "SSS-Class Suicide Hunter",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/SSS-Class_Suicide_Hunter.jpg",
+  },
+  {
+    name: "The Second Coming of Gluttony",
+    released: "2020-06-19",
+    metacritic: 90,
+    background_image: "images/The_Second_Coming_of_Gluttony.jpg",
+  },
+];
+function renderGameThumbnail({ name, background_image, released, metacritic }) {
+  const releasedDate = new Date(released);
+  return `<a href="${background_image}">
+			<img src="${background_image}" />
+			<footer>
+				<h3>${name}</h3>
+				<div class="infos">
+					<time datetime="${released}">${releasedDate.toLocaleDateString()}</time>
+					<span class="metacritic">${metacritic}</span>
+				</div>
+			</footer>
+		</a>`;
+}
+let html = "";
+data.forEach((game) => (html += renderGameThumbnail(game)));
+
+document.querySelector(".results").innerHTML = html;
